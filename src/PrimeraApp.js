9dc82b19118a11ff0ef8c9b6130f0831b130ffd8
Your Name <bpena@dubeetech.com>
2020-09-08 14:59:28 -0400

    seccion lista

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -2,20 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import React, { Fragment } from 'react';
 
-// FC
 const PrimeraApp = ({ saludo, subtitulo }) => {
-     
-   
 
     return (
-        <>       
-            <h1>{ saludo }</h1>
-            {/* <pre> { JSON.stringify( saludo, null, 3 ) } </pre> */}
-            <p>{ subtitulo }</p>
+        <>
+            <h1> { saludo } </h1>
+            {/* <pre> {  JSON.stringify( saludo, null, 3 )  } </pre> */}
+            <p> { subtitulo } </p>
         </>
-    ); 
-
-}
+    );
+        
+} 
 
 PrimeraApp.propTypes = {
     saludo: PropTypes.string.isRequired
@@ -24,5 +21,6 @@ PrimeraApp.propTypes = {
 PrimeraApp.defaultProps = {
     subtitulo: 'Soy un subtitulo'
 }
+
 
 export default PrimeraApp;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+// import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
 import './index.css';
 
 
@@ -8,4 +9,4 @@ import './index.css';
 const divRoot = document.querySelector('#root');
 
 
-ReactDOM.render( <PrimeraApp saludo="Hola" />, divRoot );
+ReactDOM.render( <CounterApp value={ 10 } />, divRoot );
